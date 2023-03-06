49cf4e45ed153d9cd2567cd74664cb58ede6f6f0
karib <karib@DESKTOP-HF351DU>
2023-03-06 22:53:32 +0900

    指摘事項修正(メッセージ→タスクへ変更)

--- src/controllers/UpdateServlet.java
+++ src/controllers/UpdateServlet.java
@@ -39,8 +39,8 @@ public class UpdateServlet extends HttpServlet {
         if(_token != null && _token.equals(request.getSession().getId())) {
             EntityManager em = DBUtil.createEntityManager();
 
-            // セッションスコープからメッセージのIDを取得して
-            // 該当のIDのメッセージ1件のみをデータベースから取得
+            // セッションスコープからタスクのIDを取得して
+            // 該当のIDのタスク1件のみをデータベースから取得
             Task m = em.find(Task.class, (Integer)(request.getSession().getAttribute("task_id")));
 
             // フォームの内容を各フィールドに上書き
@@ -55,7 +55,7 @@ public class UpdateServlet extends HttpServlet {
             if(errors.size() > 0) {
                 em.close();
 
-                // フォームに初期値を設定、さらにエラーメッセージを送る
+                // フォームに初期値を設定、さらにエラータスクを送る
                 request.setAttribute("_token", request.getSession().getId());
                 request.setAttribute("task", m);
                 request.setAttribute("errors", errors);
